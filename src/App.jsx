88bfd19f4e4d8f5644f8a6f673331b65dffddb88
Lili <liliana.zurita@gmail.com>
2023-01-08 01:33:59 -0300

    componente  button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
 const App = () => {
+    const userCredential={
+        email:"lili@mail.cl",
+        password:"qwerty"
+    }
     const handleSubmit = (e) => { 
         e.preventDefault();
-        console.log('hola click')
+        if(!email.trim()&&!password.trim()){
+            alert("Llene los campos")
+        }
+        console.log({email,password})
     };
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <input onChange={(e)=>{console.log(e.target.value)}} type="text" placeholder="email"></input>
-                <input type="password" placeholder="password"></input>
-                <button type="submit">Submit</button>
+                <input onChange={(e)=>{setEmail(e.target.value)}} type="text" placeholder="email"></input>
+                <input onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder="password"></input>
+                <button type="submit" disabled >Submit</button>
             </form>
+            {password==="252525"&&(<Button/>)}
+            
+            <p>{email}-{password}</p>
         </>
+
     );
 };
 
